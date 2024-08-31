7c5c7820fac59257c80cc0e72d0c0ee52ef2efbd
nikashitsa <vernik91@gmail.com>
2024-08-31 15:01:45 +0200

    chore: lint

--- test/BitcoinWallet.test.js
+++ test/BitcoinWallet.test.js
@@ -6,8 +6,6 @@ import { Amount, Transaction } from '@coinspace/cs-common';
 import * as utils from './utils.js';
 import Wallet from '@coinspace/cs-bitcoin-wallet';
 
-import crypto from "crypto";
-
 // eslint-disable-next-line max-len
 const RANDOM_SEED = Buffer.from('2b48a48a752f6c49772bf97205660411cd2163fe6ce2de19537e9c94d3648c85c0d7f405660c20253115aaf1799b1c41cdd62b4cfbb6845bc9475495fc64b874', 'hex');
 const RANDOM_SEED_PUB_KEY = {
